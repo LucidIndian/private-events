64165ee3048771be38b08c6cae092c389bc69c96
Tygh Walters <tyghwalters@gmail.com>
2023-01-30 17:52:01 -0800

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "events#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'events/index'
 end
