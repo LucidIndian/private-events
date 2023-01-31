3319ad7641310138cf68860134b27f591baf4e39
Tygh Walters <tyghwalters@gmail.com>
2023-01-31 08:45:07 -0800

    Revert "Yellow Background First CSS and Add RsvpsController"
    This reverts commit 720e6fc3a39f40762d60c0f211e3bd59d8d29a5c.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'rsvps/index'
-  get 'rsvps/accept'
-  get 'rsvps/refuse'
-
   get 'users/show'
   
   devise_for :users
   root to: "events#index"
 
   resources :events, only: [ :new, :create, :show, :index ]
-  
-  resources :rsvps
 end
