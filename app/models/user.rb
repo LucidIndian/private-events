7da9b4a5df8f2b668608b00f00757182fffe3304
Tygh Walters <tyghwalters@gmail.com>
2023-01-31 21:17:56 -0800

    Add to the Users#Show View a List of the RSVPd Events

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
   has_many :created_events, foreign_key: :creator_id, class_name: "Event", inverse_of: :creator
   has_many :rsvps, foreign_key: :attendee_id
-  has_many :attended_events, through: :rsvps, source: :event
+  # has_many :attended_events, through: :rsvps, source: :event
+  has_many :attended_events, through: :rsvps
 end
