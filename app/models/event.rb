102b14c05cb9b9da239b3b367bae99120520636d
Tygh Walters <tyghwalters@gmail.com>
2023-01-31 22:12:50 -0800

    Add Future and Past Event Sections to Users#shoe View

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  # attr_accessor :
+
   belongs_to :creator, class_name: "User"
   has_many :rsvps, foreign_key: :attended_event_id
   has_many :attendees, through: :rsvps
